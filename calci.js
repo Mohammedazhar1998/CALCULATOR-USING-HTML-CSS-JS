28bdce6a763caf5a89e0c7f15993149ff78129e9
mohammedazhar1998 <azharmohammed612@gmail.com>
2023-08-23 19:31:41 +0530

    First commit

---
+++ calci.js
@@ -0,0 +1,16 @@
+var screen = document.getElementById('screen');
+
+function btnclick(value){
+    screen.value += value;
+}
+
+
+function clearScreen() {
+    screen.value ="";
+}
+
+function findResult(){
+    var result = eval(screen.value)
+    screen.value = result;
+
+}
